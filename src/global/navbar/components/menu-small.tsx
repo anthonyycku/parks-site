7a54e7a9340bee11c47e0407021a63709eae806e
Anthony <anthony.yc.ku@gmail.com>
2023-03-06 23:13:00 -0500

    added menu to navbar

--- src/global/navbar/components/menu-small.tsx
+++ src/global/navbar/components/menu-small.tsx
@@ -3,6 +3,7 @@ import { AiOutlineMenu } from "react-icons/all";
 import navMenu from "../constants/nav-menu";
 import MenuButton from "../../button/menu-button";
 import { Link } from "react-router-dom";
+import '../nav-bar.css';
 
 const MenuSmall = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -27,7 +28,7 @@ const MenuSmall = () => {
   }, [])
 
   return (
-    <div className="relative" ref={menuRef}>
+    <div className={`relative`} ref={menuRef}>
       <div
         className={`absolute left-[1rem] top-0 transform -translate-x-1/2 -translate-y-1/2 ${isOpen ? 'bg-opacity-50 bg-gray-300 rounded-full p-2' : ''}`}>
         <AiOutlineMenu
@@ -38,7 +39,8 @@ const MenuSmall = () => {
       </div>
 
       {isOpen && (
-        <div className={`absolute top-8 -left-2 w-32 bg-black rounded p-2 bg-opacity-90 flex flex-col space-y-4`}>
+        <div
+          className={`menu ${isOpen ? 'menu-open ' : ''} absolute  w-32 bg-black rounded p-2 bg-opacity-90 flex flex-col space-y-4`}>
           {navMenu.map(({ path, text }) => (
             <Link to={path} key={text} onClick={handleMenuClick}>
               <MenuButton text={text}/>
